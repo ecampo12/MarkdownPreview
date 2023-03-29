bab6c1424c520ac677f68a923ff0844bf4a95ba9
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-03-29 14:51:03 -0400

    added PreviewGUi

---
+++ GUI/PreviewGUI.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"io/ioutil"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+
+	"github.com/russross/blackfriday/v2"
+)
+
+func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Markdown Preview")
+
+	mdEntry := widget.NewMultiLineEntry()
+	mdEntry.Resize(fyne.NewSize(400, 400))
+
+	htmlLabel := widget.NewLabel("")
+	htmlLabel.Wrapping = fyne.TextWrapWord
+	htmlLabel.Resize(fyne.NewSize(400, 400))
+
+	// Toolbar items
+	openMenuItem := fyne.NewMenuItem("Open", func() {
+		dialog.ShowFileOpen(func(reader fyne.URIReadCloser, err error) {
+			if err == nil && reader != nil {
+				bytes, err := ioutil.ReadAll(reader)
+				if err == nil {
+					mdEntry.SetText(string(bytes))
+				}
+				reader.Close()
+			}
+		}, myWindow)
+	})
+
+	saveMenuItem := fyne.NewMenuItem("Save", func() {
+		dialog.ShowFileSave(func(writer fyne.URIWriteCloser, err error) {
+			if err == nil && writer != nil {
+				_, err := writer.Write([]byte(mdEntry.Text))
+				if err != nil {
+					dialog.ShowError(err, myWindow)
+				}
+				writer.Close()
+			}
+		}, myWindow)
+	})
+
+	mainMenu := fyne.NewMenu(
+		"File",
+		openMenuItem,
+		saveMenuItem,
+	)
+
+	mdEntry.OnChanged = func(text string) {
+		html := blackfriday.Run([]byte(text))
+		htmlLabel.SetText(string(html))
+	}
+
+	content := container.New(layout.NewGridLayoutWithColumns(2), mdEntry, htmlLabel)
+	myWindow.SetContent(content)
+	myWindow.Resize(fyne.NewSize(400, 400))
+	myWindow.SetMainMenu(fyne.NewMainMenu(mainMenu))
+	myWindow.ShowAndRun()
+}
--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
@@ -29,6 +30,7 @@ require (
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
